c62ac7990341fc8a359008b0b47daf73bfc2d116
Kuba429 <jakubzawadzki429@gmail.com>
2023-05-24 16:13:41 +0200

    initial navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,24 @@ export default function RootLayout({
 
 const Navbar = () => {
 	return (
-		<nav className="bg-red-500 p-5">
+		<nav className="flex px-3 items-center bg-red-500 justify-between">
 			<span className="text-xl font-bold text-neutral-50">
 				SuperSupply
 			</span>
+			<div className="flex rounded m-3 flex-grow box-content">
+				<input
+					type="text"
+					className="rounded-l px-1 w-full"
+					placeholder="Search..."
+				></input>
+				<div className="aspect-square px-3 rounded-r bg-neutral-200 flex items-center justify-center text-center">
+					L
+				</div>
+				{
+				// replace L and C with icons
+				}
+			</div>
+			<div className="text-neutral-50 aspect-square">C</div>
 		</nav>
 	);
 };
