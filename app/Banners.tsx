e4b886699fc0030d813b62ef5d43f3514f83d6f7
Kuba429 <jakubzawadzki429@gmail.com>
2023-05-25 15:17:59 +0200

    responsive banners on landing page

--- app/Banners.tsx
+++ app/Banners.tsx
@@ -1,60 +1,71 @@
 "use client";
 import { BsArrowRightShort } from "react-icons/bs";
 export default function () {
-	return <ApparelBanner />;
+	// TODO carousel
+	return (
+		<>
+			<ApparelBanner />
+			<WeaponsBanner />
+			<ArtifactsBanner />
+		</>
+	);
+}
+
+function ApparelBanner() {
+	return (
+		<section className="m-5 grid h-96 grid-cols-2 grid-rows-[1fr_auto_1fr] place-items-start gap-x-5 gap-y-5">
+			<img
+				src="banners/starlord.webp"
+				className="row-start-1 row-end-4 max-h-full place-self-center object-contain"
+				alt="Starlord"
+			/>
+			<h1 className="grid-row-1 self-end text-3xl font-extrabold md:text-6xl">
+				Apparel
+			</h1>
+			<p className="grid-row-2 max-w-sm self-center text-left">
+				Step into greatness with our remarkable range of costumes and
+				suits, tailored for extraordinary heroes
+			</p>
+			<button className="group flex items-center rounded bg-red-500 px-3 py-2 text-xl text-white">
+				Shop now
+				<BsArrowRightShort className="transition-transform group-hover:translate-x-1" />
+			</button>
+		</section>
+	);
 }
 
 function WeaponsBanner() {
 	return (
-		<section className="h-96 flex items-center justify-center gap-12">
-			<div className="flex justify-center flex-col gap-5">
-				<h1 className="text-6xl font-extrabold">Weapons</h1>
-				<p className="max-w-sm">
-					Shop for the most reliable weapons on the market
-				</p>
-				<ShopNowButton />
-			</div>
+		<section className="m-5 grid h-96 grid-cols-2 grid-rows-[1fr_auto_1fr] place-items-end items-start gap-x-5 gap-y-5">
+			<h1 className="self-end text-3xl font-extrabold md:text-6xl">
+				Weapons
+			</h1>
+			<p className="grid-row-2 max-w-sm self-center text-right">
+				Shop for the most reliable weapons on the market
+			</p>
+			<ShopNowButton />
 			<img
 				src="banners/thors-hammer.png"
-				className="h-1/2 object-contain rotate-45"
+				className="col-start-2 row-start-1 row-end-4 max-h-full rotate-45 place-self-center object-contain"
 				alt="Thor's hammer"
 			/>
 		</section>
 	);
 }
 
-function ApparelBanner() {
-	return (
-		<section className="h-96 flex items-center justify-center gap-12">
-			<img
-				src="banners/starlord.webp"
-				className="h-full object-contain"
-				alt="Starlord"
-			/>
-			<div className="flex justify-center items-end flex-col gap-5">
-				<h1 className="text-6xl font-extrabold">Apparel</h1>
-				<p className="max-w-sm text-right">
-					Step into greatness with our remarkable range of costumes
-					and suits, tailored for extraordinary heroes
-				</p>
-				<ShopNowButton />
-			</div>
-		</section>
-	);
-}
 function ArtifactsBanner() {
 	return (
-		<section className="h-96 flex items-center justify-center gap-12">
-			<div className="flex justify-center items-start flex-col gap-5">
-				<h1 className="text-6xl font-extrabold">Artifacts</h1>
-				<p className="max-w-sm">
-					Buy some of the most powerful objects in the universe!
-				</p>
-				<ShopNowButton />
-			</div>
+		<section className="m-5 grid h-96 grid-cols-2 grid-rows-[1fr_auto_1fr] place-items-start gap-x-5 gap-y-5">
+			<h1 className="grid-row-1 self-end text-3xl font-extrabold md:text-6xl">
+				Artifacts
+			</h1>
+			<p className="grid-row-2 max-w-sm self-center text-left">
+				Buy some of the most powerful objects in the universe!
+			</p>
+			<ShopNowButton />
 			<img
 				src="banners/gauntlet.webp"
-				className="h-2/3 object-contain"
+				className="row-start-1 row-end-4 max-h-full place-self-center object-contain"
 				alt="Infinity Gauntlet"
 			/>
 		</section>
@@ -62,9 +73,9 @@ function ArtifactsBanner() {
 }
 const ShopNowButton = () => {
 	return (
-		<button className="group flex items-center rounded bg-red-500 text-white text-xl px-3 py-2">
+		<button className="group flex items-center rounded bg-red-500 px-3 py-2 text-xl text-white">
 			Shop now
-			<BsArrowRightShort className="group-hover:translate-x-1 transition-transform" />
+			<BsArrowRightShort className="transition-transform group-hover:translate-x-1" />
 		</button>
 	);
 };
