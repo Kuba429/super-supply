57da4167549b00b390acb35c9cc13dfa80e5e74c
Kuba429 <jakubzawadzki429@gmail.com>
2023-05-29 19:56:27 +0200

    use reusable caousel for testimonials

--- app/Carousel.tsx
+++ app/Carousel.tsx
@@ -12,10 +12,6 @@ export function Carousel({
 }) {
 	const slidesCount = React.Children.count(children);
 	const [currentSlide, setCurrentSlide] = useState(0);
-	const style: CSSProperties = {
-		transform: `translateX(-${currentSlide * 100}vw)`,
-		transition: "transform 0.3s ease",
-	};
 	useEffect(() => {
 		const interval = setInterval(
 			() => setCurrentSlide((s) => (s + 1) % slidesCount),
@@ -23,12 +19,17 @@ export function Carousel({
 		);
 		return () => clearInterval(interval);
 	}, [setCurrentSlide, currentSlide]); // added slide as dep for when user explicitly changes slide
+	const slideStyle: CSSProperties = {
+		transform: `translateX(-${currentSlide * 100}vw)`,
+		transition: "transform 0.3s ease",
+	};
 	return (
-		<div className={`grid grid-cols-[repeat(${slidesCount},100vw)]`}>
-			{React.Children.map(children, (c: ReactNode) => (
-				<div style={style} className={``}>
-					{c}
-				</div>
+		<div
+			style={{ gridTemplateColumns: `repeat(${slidesCount},100vw)` }} // doesn't work in tailwind
+			className={`grid`}
+		>
+			{React.Children.map(children, (node: ReactNode) => (
+				<div style={slideStyle}>{node}</div>
 			))}
 		</div>
 	);
--- app/Testimonials.tsx
+++ app/Testimonials.tsx
@@ -1,54 +1,35 @@
-"use client";
 import Image from "next/image";
-import { CSSProperties, useEffect, useState } from "react";
+import { Carousel } from "./Carousel";
 export default function Testimonials() {
-	const [slide, setSlide] = useState(0);
-	const style: CSSProperties = {
-		transform: `translateX(-${slide * 100}vw)`,
-		transition: "transform 0.3s ease",
-	};
-	useEffect(() => {
-		const interval = setInterval(() => setSlide((s) => (s + 1) % 3), 5000);
-		return () => clearInterval(interval);
-	}, [setSlide, slide]); // added slide as dep for when user explicitly changes slide
 	return (
 		<>
 			<h2 className="mt-5 text-center text-4xl font-bold text-neutral-700">
 				Customer Stories: Real{" "}
 				<span className="text-red-500">Heroes</span>, Real Experiences.
 			</h2>
-			<div className="grid grid-cols-[repeat(3,100vw)] overflow-x-hidden">
-				<div style={style}>
-					<Testimonial
-						name="Tony Stark"
-						description="
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+			<Carousel>
+				<Testimonial
+					name="Tony Stark"
+					description="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
 					Itaque architecto exercitationem cupiditate minus
 					perspiciatis esse debitis nobis eligendi quasi aliquid."
-						imageSrc="/people/tony-stark.png"
-					/>
-				</div>
-				<div style={style}>
-					<Testimonial
-						name="Peter Parker"
-						description="
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					imageSrc="/people/tony-stark.png"
+				/>
+				<Testimonial
+					name="Peter Parker"
+					description="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
 					Itaque architecto exercitationem cupiditate minus
 					perspiciatis esse debitis nobis eligendi quasi aliquid."
-						imageSrc="/people/peter-parker.jpg"
-					/>
-				</div>
-				<div style={style}>
-					<Testimonial
-						name="Steve Rogers"
-						description="
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					imageSrc="/people/peter-parker.jpg"
+				/>
+				<Testimonial
+					name="Steve Rogers"
+					description="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
 					Itaque architecto exercitationem cupiditate minus
 					perspiciatis esse debitis nobis eligendi quasi aliquid."
-						imageSrc="/people/steve-rogers.jpg"
-					/>
-				</div>
-			</div>
+					imageSrc="/people/steve-rogers.jpg"
+				/>
+			</Carousel>
 		</>
 	);
 }
