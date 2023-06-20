ae2092bf260c414b4ffa1378523e1b1ae3491be9
Kuba429 <jakubzawadzki429@gmail.com>
2023-06-20 20:34:07 +0200

    split filter function into smaller functions; tests

---
+++ __test__/search/searchEngine.test.ts
@@ -0,0 +1,169 @@
+import { itemType } from "@/app/data";
+import {
+	applyParamFilters,
+	matchesCategories,
+	matchesManufacturers,
+	matchesPriceRange,
+	matchesUsedBy,
+	parseArrayParam,
+	parseSearchParams,
+	urlSearchParamsRaw,
+} from "@/app/search/searchEngine";
+
+test("parseArrayParam", () => {
+	const raw = "param1,param2,pAraM3";
+	const parsed = parseArrayParam(raw);
+	expect(parsed).toHaveLength(3);
+	expect(parsed[0]).toBe("param1");
+	expect(parsed[1]).toBe("param2");
+	expect(parsed[2]).toBe("param3");
+});
+
+test("parseSearchParams", () => {
+	const raw: urlSearchParamsRaw = {
+		categories: "caTegOry1,categorY2",
+		priceMin: 40,
+		usedBy: "somEone",
+	};
+	const parsed = parseSearchParams(raw);
+
+	expect(parsed.usedBy).toHaveLength(1);
+	expect(parsed.usedBy[0]).toBe("someone");
+	expect(parsed.categories).toHaveLength(2);
+	expect(parsed.categories[0]).toBe("category1");
+	expect(parsed.categories[1]).toBe("category2");
+	expect(parsed.priceMin).toBe(40);
+	expect(parsed.priceMax).toBe(Infinity);
+	expect(parsed.manufacturers).toHaveLength(0);
+});
+
+const items = [
+	{
+		id: "123",
+		usedBy: ["Hulk", "Spider-Man"],
+		categories: ["weapons"],
+		name: "test item",
+		price: 400,
+		image: "url",
+		manufacturer: "Oscorp",
+	},
+	{
+		id: "123",
+		usedBy: ["Spider-Man"],
+		categories: ["weapons", "suits"],
+		name: "test item2",
+		price: 9000,
+		image: "url",
+		manufacturer: "Pym Technologies",
+	},
+] satisfies itemType[];
+
+describe("ApplyParamFilters", () => {
+	it("filter by usedBy", () => {
+		const usedByHulk = applyParamFilters(items, { usedBy: "hulk" });
+		expect(usedByHulk).toHaveLength(1);
+		const usedByHawkeye = applyParamFilters(items, { usedBy: "hawkeye" });
+		expect(usedByHawkeye).toHaveLength(0);
+		const usedBySpiderman = applyParamFilters(items, {
+			usedBy: "spider-man",
+		});
+		expect(usedBySpiderman).toHaveLength(2);
+	});
+	it("filter by categories", () => {
+		const suits = applyParamFilters(items, { categories: "suits" });
+		expect(suits).toHaveLength(1);
+		const weapons = applyParamFilters(items, { categories: "wEaPOns" });
+		expect(weapons).toHaveLength(2);
+	});
+	it("filter by priceMax", () => {
+		const max1 = applyParamFilters(items, { priceMax: 5000 });
+		expect(max1).toHaveLength(1);
+
+		const max2 = applyParamFilters(items, { priceMax: 10000 });
+		expect(max2).toHaveLength(2);
+
+		const max3 = applyParamFilters(items, { priceMax: 1 });
+		expect(max3).toHaveLength(0);
+	});
+	it("filter by priceMin", () => {
+		const min1 = applyParamFilters(items, { priceMin: 500 });
+		expect(min1).toHaveLength(1);
+
+		const min2 = applyParamFilters(items, { priceMin: 100 });
+		expect(min2).toHaveLength(2);
+
+		const min3 = applyParamFilters(items, { priceMin: 99999999 });
+		expect(min3).toHaveLength(0);
+	});
+	it("filter by price range", () => {
+		const r1 = applyParamFilters(items, { priceMax: 5000, priceMin: 10 });
+		expect(r1).toHaveLength(1);
+
+		const r2 = applyParamFilters(items, {
+			priceMax: 220123000,
+			priceMin: 30,
+		});
+		expect(r2).toHaveLength(2);
+
+		const r3 = applyParamFilters(items, {
+			priceMax: 999999,
+			priceMin: 50000,
+		});
+		expect(r3).toHaveLength(0);
+	});
+});
+
+describe("Matching functions", () => {
+	it("matchesCategories", () => {
+		const matchedBySuits = matchesCategories(
+			items[0],
+			parseSearchParams({ categories: "suits" })
+		);
+		expect(matchedBySuits).toBe(false);
+
+		const matchedByWeapons = matchesCategories(
+			items[0],
+			parseSearchParams({ categories: "weapons" })
+		);
+		expect(matchedByWeapons).toBe(true);
+	});
+	it("matchesUsedBy", () => {
+		const matchedBySpidey = matchesCategories(
+			items[0],
+			parseSearchParams({ usedBy: "spider-man" })
+		);
+		expect(matchedBySpidey).toBe(true);
+
+		const matchedByUnrecorded = matchesUsedBy(
+			items[0],
+			parseSearchParams({ usedBy: "someone who doesnt exist" })
+		);
+		expect(matchedByUnrecorded).toBe(false);
+	});
+	it("machesManufacturers", () => {
+		const matchedByOscorp = matchesManufacturers(
+			items[0],
+			parseSearchParams({ manufacturers: "oscorp" })
+		);
+		expect(matchedByOscorp).toBe(true);
+
+		const matchedBySomethingElse = matchesManufacturers(
+			items[0],
+			parseSearchParams({ manufacturers: "abcasdasdadsad" })
+		);
+		expect(matchedBySomethingElse).toBe(false);
+	});
+	it("matchesPriceRange", () => {
+		const r1 = matchesPriceRange(
+			items[0],
+			parseSearchParams({ priceMin: 1, priceMax: 40001231 })
+		);
+		expect(r1).toBe(true);
+
+		const r2 = matchesPriceRange(
+			items[0],
+			parseSearchParams({ priceMin: 5, priceMax: 6 })
+		);
+		expect(r2).toBe(false);
+	});
+});
--- app/search/searchEngine.ts
+++ app/search/searchEngine.ts
@@ -1,4 +1,4 @@
-import { heroType, itemType } from "../data";
+import { itemType } from "../data";
 
 export type urlSearchParamsRaw = {
 	q?: string;
@@ -18,42 +18,57 @@ export type urlSeachParamsParsed = {
 	priceMax: number;
 };
 
-// TODO: split this into smaller functions
 export function applyParamFilters(
 	items: itemType[],
 	rawParams: urlSearchParamsRaw
 ) {
 	const params = parseSearchParams(rawParams);
 	return items.filter((item) => {
-		// reject if no match on categories
-		if (
-			params.categories.length > 0 &&
-			!item.categories.some((c) => params.categories.includes(c))
-		)
-			return false;
-		// reject if price not withing queried range
-		const itemActualPrice = item.price - item.price * (item.discount ?? 0);
-		if (params.priceMax && itemActualPrice > params.priceMax) return false;
-		if (params.priceMin && itemActualPrice < params.priceMin) return false;
-		// reject if no match on manufacturer
-		if (
-			params.manufacturers.length > 0 &&
-			!params.manufacturers.includes(
-				item.manufacturer?.toLowerCase() ?? ""
-			)
-		)
-			return false;
-		// if no match on superhero
-		if (
-			params.usedBy.length > 0 &&
-			!item.usedBy.some((h) => params.usedBy.includes(h.toLowerCase()))
-		) {
-			return false;
-		}
+		if (!matchesPriceRange(item, params)) return false;
+		if (!matchesManufacturers(item, params)) return false;
+		if (!matchesUsedBy(item, params)) return false;
+		if (!matchesCategories(item, params)) return false;
 		return true;
 	});
 }
-function parseSearchParams(params: urlSearchParamsRaw) {
+
+export function matchesPriceRange(
+	item: itemType,
+	params: urlSeachParamsParsed
+) {
+	const itemActualPrice = item.price - item.price * (item.discount ?? 0);
+	if (itemActualPrice > params.priceMax) return false;
+	if (itemActualPrice < params.priceMin) return false;
+	return true;
+}
+
+export function matchesCategories(
+	item: itemType,
+	params: urlSeachParamsParsed
+) {
+	if (params.categories.length < 1) return true;
+	if (item.categories.some((c) => params.categories.includes(c))) return true;
+	return false;
+}
+
+export function matchesUsedBy(item: itemType, params: urlSeachParamsParsed) {
+	if (params.usedBy.length < 1) return true;
+	if (item.usedBy.some((c) => params.usedBy.includes(c.toLowerCase())))
+		return true;
+	return false;
+}
+
+export function matchesManufacturers(
+	item: itemType,
+	params: urlSeachParamsParsed
+) {
+	if (params.manufacturers.length < 1) return true;
+	if (params.manufacturers.includes(item.manufacturer?.toLowerCase() ?? ""))
+		return true;
+	return false;
+}
+
+export function parseSearchParams(params: urlSearchParamsRaw) {
 	const parsed: urlSeachParamsParsed = {
 		...params,
 		usedBy: parseArrayParam(params.usedBy ?? ""),
@@ -64,7 +79,7 @@ function parseSearchParams(params: urlSearchParamsRaw) {
 	};
 	return parsed;
 }
-function parseArrayParam(param: string) {
+export function parseArrayParam(param: string) {
 	return param
 		.split(",")
 		.filter((i) => i.length)
