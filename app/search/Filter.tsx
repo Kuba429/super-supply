1b87529ac3928028c5f5dc67aaab2527d30fad0d
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-05 11:55:18 +0200

    made filtering more flexible

--- app/search/Filter.tsx
+++ app/search/Filter.tsx
@@ -1,48 +1,57 @@
 import { AccordionFilter } from "./FilterAccordion";
 import { categories, heroes, manufacturers } from "../data";
 import { redirect } from "next/navigation";
+import { urlSearchParamsRaw } from "./searchEngine";
 
 async function fetchData() {
 	return { categories, usedBy: heroes, manufacturers };
 }
 export default async function Filter() {
+	const fetchedData = await fetchData();
+	const filters: {
+		title: string;
+		paramKey: keyof urlSearchParamsRaw;
+		values: string[];
+	}[] = [
+		{
+			title: "Used by",
+			paramKey: "usedBy",
+			values: fetchedData.usedBy.map((c) => c.name),
+		},
+		{
+			title: "Categories",
+			paramKey: "categories",
+			values: fetchedData.categories.map((c) => c.name),
+		},
+		{
+			title: "Manufacturer",
+			paramKey: "manufacturers",
+			values: fetchedData.manufacturers.map((c) => c.name),
+		},
+	];
 	async function applyFiltersAction(data: FormData) {
 		"use server";
 		const params: [string, string][] = [];
-		const categoriesParam = data.getAll("categories").toString();
-		if (categoriesParam.length > 0)
-			params.push(["categories", categoriesParam]);
-
-		const usedByParams = data.getAll("usedBy").toString();
-		if (usedByParams.length > 0) params.push(["usedBy", usedByParams]);
-
-		const manufacturersParam = data.getAll("manufacturers")?.toString();
-		if (manufacturersParam.length > 0)
-			params.push(["manufacturers", manufacturersParam]);
+		for (const f of filters) {
+			const param = data.getAll(f.paramKey).toString();
+			if (param.length > 0) params.push([f.paramKey, param]);
+		}
 
 		const urlParams = new URLSearchParams(params);
 		redirect("/search?" + urlParams.toString());
 	}
-	const fetchedData = await fetchData();
 	return (
 		<div className="h-fit rounded bg-slate-100 p-3">
 			<h3 className="text-xl font-bold text-red-500">Filter</h3>
 			<form action={applyFiltersAction}>
-				<AccordionFilter
-					title={"Category"}
-					paramKey={"categories"}
-					values={fetchedData.categories.map((c) => c.name)}
-				/>
-				<AccordionFilter
-					title={"Used by"}
-					paramKey={"usedBy"}
-					values={fetchedData.usedBy.map((c) => c.name)}
-				/>
-				<AccordionFilter
-					title={"Manufacturer"}
-					paramKey={"manufacturers"}
-					values={fetchedData.manufacturers.map((c) => c.name)}
-				/>
+				{filters.map((f) => (
+					<AccordionFilter
+						title={f.title}
+						paramKey={f.paramKey}
+						values={f.values}
+					/>
+				))}
+
 				<div className="m-1 flex items-center justify-center">
 					<button
 						type="submit"
