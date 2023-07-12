84200005d997fb1cd2ce5df4140a972c6690f440
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-12 12:01:59 +0200

    fixed reducers

--- app/store.ts
+++ app/store.ts
@@ -2,18 +2,27 @@ import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
 import { itemType } from "./data";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
+const LOCAL_STORAGE_CART_KEY = "persisted-cart-state";
 export const cartSlice = createSlice({
 	name: "cart",
-	initialState: [] as itemType[],
+	initialState: getInitialCartState(),
 	reducers: {
 		remove: (state) => {
-			state = [];
+			return [];
 		},
 		addItem: (state, action: PayloadAction<itemType>) => {
-			state.push(action.payload);
+			return [...state, action.payload];
 		},
 	},
 });
+
+function getInitialCartState() {
+	const persistedState = localStorage?.getItem(LOCAL_STORAGE_CART_KEY);
+	if (!persistedState) {
+		return [] as itemType[];
+	}
+	return JSON.parse(persistedState) as itemType[];
+}
 
 export const { remove, addItem } = cartSlice.actions;
 const store = configureStore({
